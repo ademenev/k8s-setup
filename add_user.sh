ff89d2c6b130389fffd8e649586b62d8cbf8eb0d
Andrey Demenev <andrey@1crm.com>
2025-01-20 23:29:41 +0400

    Missing space

--- add_user.sh
+++ add_user.sh
@@ -4,7 +4,7 @@ KEY=$2
 adduser --disabled-password $USER
 mkdir /home/$USER/.ssh
 echo $KEY > /home/$USER/.ssh/authorized_keys
-chown -R $USER:/home/$USER/.ssh
+chown -R $USER: /home/$USER/.ssh
 chmod 0700 /home/$USER/.ssh
 chmod 0600 /home/$USER/.ssh/authorized_keys
 echo "$USER 'ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/90-$USER
